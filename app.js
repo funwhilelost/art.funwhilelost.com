c3e1fcc60e9b73cce9392d3789c0635f6306fc0b
Andrew Jacobs <andrew@flyingtrolleycars.com>
2015-11-07 15:23:14 -0800

    Switching to minimal server.

--- app.js
+++ app.js
@@ -1,69 +1,13 @@
 'use strict';
 
-/*
- * Express Dependencies
- */
+// simple express server
 var express = require('express');
 var app = express();
-var port = 3000;
+var router = express.Router();
 
-/*
- * Use Handlebars for templating
- */
-var exphbs = require('express3-handlebars');
-var hbs;
-
-// For gzip compression
-app.use(express.compress());
-
-/*
- * Config for Production and Development
- */
-if (process.env.NODE_ENV === 'production') {
-    // Set the default layout and locate layouts and partials
-    app.engine('handlebars', exphbs({
-        defaultLayout: 'main',
-        layoutsDir: 'dist/views/layouts/',
-        partialsDir: 'dist/views/partials/'
-    }));
-
-    // Locate the views
-    app.set('views', __dirname + '/dist/views');
-    
-    // Locate the assets
-    app.use(express.static(__dirname + '/dist/assets'));
-
-} else {
-    app.engine('handlebars', exphbs({
-        // Default Layout and locate layouts and partials
-        defaultLayout: 'main',
-        layoutsDir: 'views/layouts/',
-        partialsDir: 'views/partials/'
-    }));
-
-    // Locate the views
-    app.set('views', __dirname + '/views');
-    
-    // Locate the assets
-    app.use(express.static(__dirname + '/assets'));
-}
-
-// Set Handlebars
-app.set('view engine', 'handlebars');
-
-
-
-/*
- * Routes
- */
-// Index Page
-app.get('/', function(request, response, next) {
-    response.render('index');
+app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.sendfile('./public/index.html');
 });
 
-
-/*
- * Start it up
- */
-app.listen(process.env.PORT || port);
-console.log('Express started on port ' + port);
+app.listen(5000);
