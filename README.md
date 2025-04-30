0fd14c9d3be891e9c66e7125b10d4da02f2b7e7d
Andy Jacobs <andrew@flyingtrolleycars.com>
2025-04-29 19:58:35 -0700

    fix: refresh for 2025

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 ## ANSI Art Pack Browser
 
-Built for the Web 1.0 Conference 2015 in Portland, OR.  With the rise of the BBS and ANSI art scene - and [demoscene](https://en.wikipedia.org/wiki/Demoscene) - in the 90's there were thousands of art packs shared around the world as ZIP files.  This project allows you to view the contents of those art packs by pulling the ANSI files from Amazon S3 storage and displaying them in the browser as they originally appeared in the terminal at 14.4kbps - using the awesome JavaScript port of the ANSIlove library.
+Built for the Web 1.0 Conference 2015 in Portland, OR.  With the rise of the BBS and ANSI art scene - and [demoscene](https://en.wikipedia.org/wiki/Demoscene) - in the 90's there were thousands of art packs shared around the world as ZIP files.  This project allows you to view the contents of those art packs by pulling the ANSI files from Amazon S3 storage and displaying them in the browser as they originally appeared in the terminal at 14.4kbps - using the awesome JavaScript port of the [ANSIlove library](https://github.com/ansilove/ansilove.js/).
 
-[View the demo](http://digitalart.funwhilelost.com/)
+### Demo
+
+![ANSI Art Pack Browser Demo](docs/artpacks.webm.mp4)
+
+```bash
+npm install
+npm start
+```
+
+Browse to http://localhost:5001 to see the artpacks!
+
+### Artpacks
+
+Just unzip the artpacks into the "packs" directory and they'll be served up to
+the project.  You can find many more here: http://artscene.textfiles.com/artpacks/ thanks to Jason Scott @textfiles
+
--- app.js
+++ app.js
@@ -5,89 +5,90 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
-var s3 = require('s3');
+// Use Node.js built-in modules for filesystem operations
+var fs = require('fs');
+var path = require('path');
 var _ = require('lodash');
 
-// build the S3 client from environment variables
-var client = s3.createClient({
-  maxAsyncS3: 20,     // this is the default
-  s3RetryCount: 3,    // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.S3_SECRET_KEY,
-    region: "" // US Standard??
-    // any other options are passed to new AWS.S3()
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-  },
-});
+// Define the path to the packs directory
+var packsDir = path.join(__dirname, 'packs');
 
 app.use(express.static('public'));
+
 app.get('/pack/:id', function(req, res) {
-  // res.set('Content-Type', 'text/html');
-  // res.status(404).send('Sorry, we cannot find that!');
-  // res.send({ "list": [1, 2, 3] });
-
-  // omit Prefix, get back "commonprefixes"
-  var api_result = client.listObjects({
-    "s3Params": {
-      "Bucket": process.env.S3_BUCKET,
-      "Prefix": req.params.id // TODO: I don't think this can result in injection problems...
+  const packId = req.params.id;
+  const packPath = path.join(packsDir, packId);
+  
+  // Validate the path to prevent directory traversal attacks
+  const resolvedPath = path.resolve(packPath);
+  if (!resolvedPath.startsWith(packsDir)) {
+    return res.status(403).send('Forbidden');
+  }
+  
+  fs.readdir(packPath, function(err, files) {
+    if (err) {
+      return res.status(404).send('Pack not found');
     }
-  });
-
-  var object_list = [];
-
-  api_result.on('data', function (data) {
-    object_list = _.map(data.Contents, function (v) {
-      return "https://s3.amazonaws.com/" + process.env.S3_BUCKET + "/" + v.Key
+    
+    // Map files to full URLs
+    var object_list = _.map(files, function(file) {
+      // Create the URL path similar to how S3 URLs were formatted before
+      return `/packs/${packId}/${file}`;
     });
-    object_list = object_list.splice(1); // trim the "directory" off the top
-  });
-
-  api_result.on('end', function () {
-    //res.send(api_result);
+    
     res.send(object_list);
   });
 });
 
 app.get('/list', function(req, res) {
-  // res.set('Content-Type', 'text/html');
-  // res.status(404).send('Sorry, we cannot find that!');
-  // res.send({ "list": [1, 2, 3] });
-
-  // omit Prefix, get back "commonprefixes"
-  var api_result = client.listObjects({
-    "s3Params": {
-      "Bucket": "art.funwhilelost.com",
-      "Delimiter": "/"
+  fs.readdir(packsDir, function(err, files) {
+    if (err) {
+      return res.status(500).send('Error reading packs directory');
     }
-  });
-
-  var object_list = [];
-
-  api_result.on('data', function (data) {
-    object_list = _.map(data.CommonPrefixes, function (v) { return v.Prefix.replace(/\/$/, "") });
-  });
-
-  api_result.on('end', function () {
-    //res.send(api_result);
-    res.send(object_list);
+    
+    // Filter out any non-directories
+    fs.stat(path.join(packsDir, files[0]), function(err, stats) {
+      if (err) {
+        return res.status(500).send('Error reading directory stats');
+      }
+      
+      // Get directories only
+      var dirPromises = files.map(function(file) {
+        return new Promise(function(resolve, reject) {
+          fs.stat(path.join(packsDir, file), function(err, stats) {
+            if (err) reject(err);
+            else resolve({ name: file, isDirectory: stats.isDirectory() });
+          });
+        });
+      });
+      
+      Promise.all(dirPromises).then(function(results) {
+        var directories = results
+          .filter(function(item) { return item.isDirectory; })
+          .map(function(item) { return item.name; });
+        
+        res.send(directories);
+      }).catch(function(err) {
+        res.status(500).send('Error reading directory structure');
+      });
+    });
   });
 });
 
+// Serve static files from the packs directory
+app.use('/packs', express.static('packs'));
+
 app.get('/contents/:prefix', function(req, res) {
-  res.sendfile('./public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/show/:object', function(req, res) {
-  res.sendfile('./public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/', function(req, res) {
-  res.sendfile('./public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5001);
+console.log('Server started on port ' + (process.env.PORT || 5001));
